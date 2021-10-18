81e8df9ef29c67f5e0a89a9774889bf7154ee190
ksbisht <ksbisht941@gmail.com>
2021-10-18 14:08:26 +0530

    reinitialize project

---
+++ jwt-basics/server.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({path: './config.env'});
+const app = require("./app");
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+})
+.then(() => console.log("DB connection successfully!"));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
